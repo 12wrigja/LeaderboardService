0d26d6e7149ec0927ed8240121fe6589d4fce98e
James Wright <12wrigja@gmail.com>
2016-03-27 15:59:04 -0400

    Cleared up the webpage stuff.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,31 +1,11 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::group(['prefix'=>'api','middleware'=>['appkey']],function(){
     Route::post('devices', 'DeviceController@index');
     Route::post('devices/register','DeviceController@registerForDeviceId');
     Route::post('devices/login','DeviceController@signInWithCurrentDevice');
     Route::post('devices/logout','DeviceController@logOutOfCurrentDevice');
     Route::post('leaderboard','LeaderboardController@index');
+    Route::post('register','Auth\AuthController@register');
     Route::post('leaderboard/submit','LeaderboardController@addToLeaderboard');
-});
-
-Route::group(['middleware' => 'web'], function () {
-    Route::auth();
-
-    Route::get('/home', 'HomeController@index');
 });
